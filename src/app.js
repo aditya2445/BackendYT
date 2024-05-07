baf1f1b2db31e6b53152ceb7d8a6b7fd0e6efd71
adi <aditya@mail.com>
2024-05-07 17:32:25 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public")) //here public is the public folder which is created for storage of pdf,images etc in the server.....  
 app.use(cookieParser())
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/api/v1/users/register (here only register will get change not the users if you go to login then it will automatically change to users/login)
+
 export { app }
