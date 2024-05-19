bbe34d8090e9a4eb31480c8a3209e61cc89190ab
adi <aditya@mail.com>
2024-05-19 22:18:31 +0530

    add getWatchHistory Endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -393,6 +393,52 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
     )
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+            // _id:req.user._id //here id is in string format
+                _id:new mongoose.Types.ObjectId(req.iser._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"Video",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"watch history fetched successfully"))
+})
+
 export {
     registerUser,
     loginUser,
@@ -403,5 +449,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
